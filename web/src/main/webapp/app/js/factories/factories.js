d757ad6a8d17e6672726b14b026b529f48ef6429
Troy Sellers <tsellers@tsellers-ltm.internal.salesforce.com>
2016-02-18 10:16:13 +0100

    insert

--- web/src/main/webapp/app/js/factories/factories.js
+++ web/src/main/webapp/app/js/factories/factories.js
@@ -4,21 +4,21 @@ var documenterFactories = angular.module('documenterFactories',[]);
 
 documenterFactories.factory('ObjectDataFactory', ['$http', function($http) {
 	
-	return $http({method: 'GET', url:'/app/overview'});
+	return $http({method: 'GET', url:'/app/overview', cache: true});
 
 }]);
 
 documenterFactories.factory('SObjectDataFactory', ['$http', function($http) {
 	
-	return $http({method: 'GET', url:'/app/sobjects'});
+	return $http({method: 'GET', url:'/app/sobjects', cache: true});
 }]);
 
 documenterFactories.factory('ApexClassFactory', ['$http', function($http) {
 	
-	return $http({method: 'GET', url:'/app/apexClasses'});
+	return $http({method: 'GET', url:'/app/apexClasses', cache: true});
 }]);
 
 documenterFactories.factory('WorkflowFactory', ['$http', function($http) {
 	
-	return $http({method: 'GET', url:'/app/workflows'});
+	return $http({method: 'GET', url:'/app/workflows', cache: true});
 }]);
